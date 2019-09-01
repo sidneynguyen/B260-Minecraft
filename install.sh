4744d35be32fa6e5c92ea36cf064e53fc80e9cad
Sidney Nguyen <imsidthesquid@gmail.com>
2019-09-01 00:06:11 -0700

    Grant execute permissions on install

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ server_files=(
 
 for server_file in "${server_files[@]}"; do
     mv $server_file $server_dir
+    chmod u+x "$server_dir/$server_file"
     echo "Installed $server_file to $server_dir"
 done
